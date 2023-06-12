23ff1e4b2578856cd15c3cb4868d632e0247a9ea
jonathan_baraza <jbaraza46@gmail.com>
2023-06-12 21:23:43 +0300

    Text input for email

--- screens/Register.jsx
+++ screens/Register.jsx
@@ -5,19 +5,27 @@ import {
   StyleSheet,
   SafeAreaView,
   StatusBar,
+  TextInput,
+  TouchableOpacity,
 } from "react-native";
 import React from "react";
 
 const Register = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <View className="flex items-center ">
+      <View className="flex items-center px-6">
         <Image
           resizeMode="contain"
           className="w-full max-h-[30vh]"
           source={require("../assets/pic2.png")}
         />
-        <Text className="font-bold  text-2xl">Register</Text>
+        <Text className="font-bold  text-2xl mb-4">Register</Text>
+
+        <TextInput
+          inputMode=""
+          className="border px-4 py-2 border-gray-300 rounded-3xl w-full"
+          placeholder="Email"
+        />
       </View>
     </SafeAreaView>
   );
