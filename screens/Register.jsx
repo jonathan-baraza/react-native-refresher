50a81e0130ca7db08bd79576e9f2d7558b48c88b
jonathan_baraza <jbaraza46@gmail.com>
2023-06-12 21:20:18 +0300

    Page title and structure

--- screens/Register.jsx
+++ screens/Register.jsx
@@ -12,12 +12,12 @@ const Register = () => {
   return (
     <SafeAreaView style={styles.container}>
       <View className="flex items-center ">
-        <Text className="">Register</Text>
         <Image
           resizeMode="contain"
           className="w-full max-h-[30vh]"
           source={require("../assets/pic2.png")}
         />
+        <Text className="font-bold  text-2xl">Register</Text>
       </View>
     </SafeAreaView>
   );
