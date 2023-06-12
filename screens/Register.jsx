25d7b350c109724fa8ff38b2d7e1311909553acb
jonathan_baraza <jbaraza46@gmail.com>
2023-06-12 21:19:25 +0300

    fix: safe area view

--- screens/Register.jsx
+++ screens/Register.jsx
@@ -1,16 +1,25 @@
-import { View, Image, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Image,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+} from "react-native";
 import React from "react";
 
 const Register = () => {
   return (
-    <View className="flex items-center ">
-      <Text className="">Register</Text>
-      <Image
-        resizeMode="contain"
-        className="w-full max-h-[30vh]"
-        source={require("../assets/pic2.png")}
-      />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View className="flex items-center ">
+        <Text className="">Register</Text>
+        <Image
+          resizeMode="contain"
+          className="w-full max-h-[30vh]"
+          source={require("../assets/pic2.png")}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -19,5 +28,6 @@ export default Register;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
   },
 });
