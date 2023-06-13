b5a8b7826c2987f9e74d68d392f7a053375a5686
jonathan_baraza <jbaraza46@gmail.com>
2023-06-13 17:31:03 +0300

    Note item components

---
+++ components/notes/NoteItem.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const NoteItem = ({ note }) => {
+  return (
+    <View className="border-2 border-red-300 mt-3 rounded-xl p-3 bg-white">
+      <Text>{note}</Text>
+    </View>
+  );
+};
+
+export default NoteItem;
