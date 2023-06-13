44c13b6310cdc850c282144407a4a646168a2ac3
jonathan_baraza <jbaraza46@gmail.com>
2023-06-13 16:57:45 +0300

    Add note input

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-import { Text, View, StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  Button,
+  StatusBar,
+} from "react-native";
 import Register from "./screens/Register";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Register />
+      <View className="px-4 py-8 flex-row space-x-2 items-center justify-between">
+        <TextInput
+          placeholder="Add your note here"
+          className="border py-2 px-3 flex-1 rounded-xl  mr-2 border-gray-400"
+        />
+        <Button title="ADD NOTE" />
+      </View>
     </View>
   );
 }
@@ -12,5 +25,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
   },
 });
