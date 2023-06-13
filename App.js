d7eeeffccf5c1ed237dc3ba6071e92d74ce76874
jonathan_baraza <jbaraza46@gmail.com>
2023-06-13 17:31:20 +0300

    fix: note render error

--- App.js
+++ App.js
@@ -5,18 +5,34 @@ import {
   TextInput,
   Button,
   StatusBar,
+  FlatList,
 } from "react-native";
 import Register from "./screens/Register";
+import NoteItem from "./components/notes/NoteItem";
+import { useState } from "react";
 
 export default function App() {
+  const [notes, setNotes] = useState(["one", "two"]);
   return (
     <View style={styles.container}>
-      <View className="px-4 py-8 flex-row space-x-2 items-center justify-between">
+      <View className="my-4 flex flex-row justify-center">
+        <Text className="mr-2 font-bold text-3xl">Nota~Bella</Text>
+        <Text className="font-bold text-3xl">📖</Text>
+      </View>
+      <View className="px-4 pb-8 pt-2 flex-row space-x-2 items-center justify-between">
         <TextInput
           placeholder="Add your note here"
           className="border py-2 px-3 flex-1 rounded-xl  mr-2 border-gray-400"
         />
         <Button title="ADD NOTE" />
+      </View>
+      <View className="bg-red-100 flex-1 px-6">
+        <FlatList
+          data={notes}
+          keyExtractor={(item, index) => index}
+          renderItem={({ item }) => <NoteItem note={item} />}
+        />
+        {/* {notes && notes.map((note, index) => <NoteItem note={note} />)} */}
       </View>
     </View>
   );
