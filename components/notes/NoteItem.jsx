b6b3956cdefa74e015960c1de6822f0975edf9d6
jonathan_baraza <jbaraza46@gmail.com>
2023-06-14 16:22:57 +0300

    Pass back index

--- components/notes/NoteItem.jsx
+++ components/notes/NoteItem.jsx
@@ -1,9 +1,9 @@
 import { View, Text, TouchableWithoutFeedback } from "react-native";
 import React from "react";
 
-const NoteItem = ({ note, onPress }) => {
+const NoteItem = ({ note, onPress, index }) => {
   return (
-    <TouchableWithoutFeedback onPress={onPress}>
+    <TouchableWithoutFeedback onPress={() => onPress(index)}>
       <View className="border-2 border-red-300 mt-3 rounded p-3 bg-white">
         <Text>{note}</Text>
       </View>
