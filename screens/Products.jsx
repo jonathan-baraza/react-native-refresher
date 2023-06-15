49ed376a6607f8a43dd2b21d9d6d5f97a5c0d349
jonathan_baraza <jbaraza46@gmail.com>
2023-06-15 11:58:58 +0300

    api call to products

--- screens/Products.jsx
+++ screens/Products.jsx
@@ -1,50 +1,26 @@
-import { View, Text, StyleSheet, FlatList, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Pressable,
+  ActivityIndicator,
+} from "react-native";
 import { useState, useEffect, useRef } from "react";
 import ProductItem from "../components/products/ProductItem";
+import axios from "axios";
 
 const Products = () => {
-  const [allProducts, setAllProducts] = useState([
-    {
-      id: 1,
-      title: "iPhone 9",
-      description: "An apple mobile which is nothing like apple",
-      price: 549,
-      discountPercentage: 12.96,
-      rating: 4.69,
-      stock: 94,
-      brand: "Apple",
-      category: "smartphones",
-      thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
-      images: [
-        "https://i.dummyjson.com/data/products/1/1.jpg",
-        "https://i.dummyjson.com/data/products/1/2.jpg",
-        "https://i.dummyjson.com/data/products/1/3.jpg",
-        "https://i.dummyjson.com/data/products/1/4.jpg",
-        "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
-      ],
-    },
-    {
-      id: 2,
-      title: "iPhone X",
-      description:
-        "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
-      price: 899,
-      discountPercentage: 17.94,
-      rating: 4.44,
-      stock: 34,
-      brand: "Apple",
-      category: "smartphones",
-      thumbnail: "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
-      images: [
-        "https://i.dummyjson.com/data/products/2/1.jpg",
-        "https://i.dummyjson.com/data/products/2/2.jpg",
-        "https://i.dummyjson.com/data/products/2/3.jpg",
-        "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
-      ],
-    },
-  ]);
+  const [allProducts, setAllProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   // const scrollRef = useRef(null);
+
+  const fetchProducts = async () => {
+    const response = await axios.get("https://dummyjson.com/products");
+    setAllProducts(response.data.products);
+    setLoading(false);
+  };
 
   const handleDeleteProduct = (index) => {
     setAllProducts(allProducts.filter((_, idx) => idx !== index));
@@ -57,29 +33,49 @@ const Products = () => {
     ]);
   };
 
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   // useEffect(() => {
   //   scrollRef?.current?.scrollToEnd({ animated: true });
   // }, [allProducts]);
 
+  //   if (loading)
+  //     return (
+  //       <View className="flex-1 justify-center items-center">
+  //         <Text className="mb-3 text-lg">Please wait...</Text>
+  //         <ActivityIndicator size={"large"} />
+  //       </View>
+  //     );
+
   return (
     <View style={styles.container}>
       <View className="my-4 flex flex-row justify-center">
-        <Text className="mr-2 font-bold text-3xl">My products</Text>
-        <Text className="font-bold text-3xl">🛒</Text>
+        <Text className="mr-2 font-bold text-2xl">My products</Text>
+        <Text className="font-bold text-2xl">🛒</Text>
       </View>
 
       <View className="bg-gray-100 flex-1 px-6 py-4">
         {/* RenderItem's param must be called Item??? */}
-        <FlatList
-          scrollToOverflowEnabled
-          data={allProducts}
-          keyExtractor={(item, index) => index}
-          renderItem={({ item, index }) => (
-            <Pressable onPress={() => handleProductPressed(index)}>
-              <ProductItem product={item} />
-            </Pressable>
-          )}
-        />
+        {loading ? (
+          <View className="flex-1 justify-center items-center">
+            <Text className="mb-3 text-lg">Fetching products...</Text>
+            <ActivityIndicator size={"large"} />
+          </View>
+        ) : (
+          <FlatList
+            showsHorizontalScrollIndicator={false}
+            scrollToOverflowEnabled
+            data={allProducts}
+            keyExtractor={(item, index) => index}
+            renderItem={({ item, index }) => (
+              <Pressable onPress={() => handleProductPressed(index)}>
+                <ProductItem product={item} />
+              </Pressable>
+            )}
+          />
+        )}
       </View>
     </View>
   );
