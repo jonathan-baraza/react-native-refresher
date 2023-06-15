f7b029ca9f02d51d8dfaeca7723eecb9714efb2a
jonathan_baraza <jbaraza46@gmail.com>
2023-06-15 11:15:41 +0300

    mt check

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
 import React from "react";
 import Notes from "./screens/Notes";
 
 const App = () => {
   return (
-    <View className="flex-1">
+    <View className="flex-1" style={styles.container}>
       <Notes />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+});
 
 export default App;
