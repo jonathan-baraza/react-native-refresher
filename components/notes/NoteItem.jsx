c07ff0e54428ff27de08fe31077da7676e6a1782
jonathan_baraza <jbaraza46@gmail.com>
2023-06-15 11:01:14 +0300

    props cleanup

--- components/notes/NoteItem.jsx
+++ components/notes/NoteItem.jsx
@@ -1,13 +1,13 @@
 import { View, Text, TouchableWithoutFeedback } from "react-native";
 import React from "react";
 
-const NoteItem = ({ note, onPress, index }) => {
+const NoteItem = ({ note }) => {
   return (
-    <TouchableWithoutFeedback onPress={() => onPress(index)}>
-      <View className="border-2 border-red-300 mt-3 rounded p-3 bg-white">
-        <Text>{note}</Text>
-      </View>
-    </TouchableWithoutFeedback>
+    // <TouchableWithoutFeedback onPress={() => onPress(index)}>
+    <View className="border-2 border-red-300 mt-3 rounded p-3 bg-white">
+      <Text>{note}</Text>
+    </View>
+    // </TouchableWithoutFeedback>
   );
 };
 
