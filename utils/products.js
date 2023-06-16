68e54fabaae005eecb1d3a93fffeb4e1acfb96c4
jonathan_baraza <jbaraza46@gmail.com>
2023-06-16 16:44:33 +0300

    handle error

--- utils/products.js
+++ utils/products.js
@@ -1,8 +1,13 @@
 export const fetchAllProducts = async () => {
-  const response = await axios.get("https://dummyjson.com/products");
-  if (response) {
-    return response.data.products;
-  } else {
+  try {
+    const response = await axios.get("https://dummyjson.com/products");
+    if (response) {
+      return response.data.products;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.log(error.message);
     return false;
   }
   // scrollRef.current.scrollToEnd({ animated: true });
