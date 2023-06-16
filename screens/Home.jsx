1bab792f8aa13f6af3171cecf3ffd79735aefb88
jonathan_baraza <jbaraza46@gmail.com>
2023-06-16 16:54:30 +0300

    fetch products util

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,7 +1,21 @@
 import { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { fetchAllProducts } from "../utils/products";
 
 export default function componentName() {
+  const [allProducts, setAllProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const products = fetchAllProducts();
+    if (products) {
+      setAllProducts(products);
+    } else {
+      setAllProducts(null);
+    }
+  };
+  useEffect(() => {
+    fetchAllProducts();
+  }, []);
   return (
     <View>
       <Text>Home</Text>
