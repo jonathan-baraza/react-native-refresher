a541bd3943f3126588559e31a9308d096650cb72
jonathan_baraza <jbaraza46@gmail.com>
2023-06-18 15:36:43 +0300

    fix: product index from params

--- components/products/productDetails/index.jsx
+++ components/products/productDetails/index.jsx
@@ -1,10 +1,13 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Button } from "react-native";
+import React, { useEffect } from "react";
 
-const ProductDetails = () => {
+const ProductDetails = ({ route, navigation }) => {
+  const { itemIndex } = route.params;
+
+  useEffect(() => {}, []);
   return (
     <View>
-      <Text>ProductDetails</Text>
+      <Text>ProductDetails {itemIndex}</Text>
     </View>
   );
 };
