e85a052058b2f9dc3f0993b5363106d0199cf1e3
jonathan_baraza <jbaraza46@gmail.com>
2023-06-18 15:16:02 +0300

    code cleanup

--- components/products/productListItem/index.jsx
+++ components/products/productListItem/index.jsx
@@ -10,7 +10,7 @@ const ProductListItem = ({ item, color }) => {
       activeOpacity={0.7}
       className={`w-[150px] m-2 rounded-lg shadow-xl shadow-gray-950  items-center justify-center px-4  h-[200px] border border-gray-200`}
     >
-      <Text className="text-center text-white">{item.title}</Text>
+      <Text className="text-center font-bold text-white">{item.title}</Text>
     </TouchableOpacity>
   );
 };
