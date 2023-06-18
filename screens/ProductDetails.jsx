86f9a7b3247fea011e199f01d745aa75b15d884d
jonathan_baraza <jbaraza46@gmail.com>
2023-06-18 15:36:55 +0300

    fetch product index from param

--- screens/ProductDetails.jsx
+++ screens/ProductDetails.jsx
@@ -1,10 +1,18 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Button } from "react-native";
+import React, { useEffect } from "react";
 
-const ProductDetails = () => {
+const ProductDetails = ({ route, navigation }) => {
+  const { itemIndex } = route.params;
+
+  useEffect(() => {
+    console.log("route.params");
+    console.log(route.params);
+  }, []);
   return (
     <View>
-      <Text>ProductDetails</Text>
+      <Text>ProductDetails {itemIndex}</Text>
+      <Text>huhu</Text>
+      <Button onPress={() => console.log(route.params)} title="press me" />
     </View>
   );
 };
