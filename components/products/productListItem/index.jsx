7b22990a0c4deb8a209dc036ae24c078c4f556f6
jonathan_baraza <jbaraza46@gmail.com>
2023-06-18 15:09:00 +0300

    text color and padding

--- components/products/productListItem/index.jsx
+++ components/products/productListItem/index.jsx
@@ -1,13 +1,16 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 
-const ProductListItem = ({ item }) => {
+const ProductListItem = ({ item, color }) => {
   return (
     <TouchableOpacity
+      style={{
+        backgroundColor: color,
+      }}
       activeOpacity={0.7}
-      className={`w-[150px] m-2 rounded-lg shadow-xl shadow-gray-950  items-center justify-center  h-[200px] border border-gray-200`}
+      className={`w-[150px] m-2 rounded-lg shadow-xl shadow-gray-950  items-center justify-center px-4  h-[200px] border border-gray-200`}
     >
-      <Text className="text-center">{item.title}</Text>
+      <Text className="text-center text-white">{item.title}</Text>
     </TouchableOpacity>
   );
 };
