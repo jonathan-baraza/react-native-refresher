bdd7e7a2ed7e41fa9602b2811fe58da33377821b
jonathan_baraza <jbaraza46@gmail.com>
2023-06-18 16:01:13 +0300

    pressable ripple effect

--- components/products/productListItem/index.jsx
+++ components/products/productListItem/index.jsx
@@ -3,7 +3,8 @@ import React from "react";
 
 const ProductListItem = ({ item, color, onPress }) => {
   return (
-    <TouchableOpacity
+    <Pressable
+      android_ripple={{ color: "#ced474" }}
       onPress={onPress}
       style={{
         backgroundColor: color,
@@ -12,7 +13,7 @@ const ProductListItem = ({ item, color, onPress }) => {
       className={`w-[150px] m-2 rounded-lg shadow-xl shadow-gray-950  items-center justify-center px-4  h-[200px] border border-gray-200`}
     >
       <Text className="text-center font-bold text-white">{item.title}</Text>
-    </TouchableOpacity>
+    </Pressable>
   );
 };
 
