20e2c9b7cdff3482a1b963f35a4e3f7c578dbf22
jonathan_baraza <jbaraza46@gmail.com>
2023-06-19 15:26:39 +0300

    fn: initial states in details page

--- components/products/productDetails/index.jsx
+++ components/products/productDetails/index.jsx
@@ -1,10 +1,16 @@
+import { useEffect, useState } from "react";
 import { View, Text, Button } from "react-native";
-import React, { useEffect } from "react";
+import { fetchProductDetails } from "../../../utils/products";
 
 const ProductDetails = ({ route, navigation }) => {
   const { itemIndex } = route.params;
+  const [product, setProduct] = useState("");
 
-  useEffect(() => {}, []);
+  const fetchProduct = async () => {};
+
+  useEffect(() => {
+    fetchProduct();
+  }, []);
   return (
     <View>
       <Text>ProductDetails {itemIndex}</Text>
