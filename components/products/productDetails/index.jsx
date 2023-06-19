dd24945ebda870b69205e40f0d548fec25d645b8
jonathan_baraza <jbaraza46@gmail.com>
2023-06-19 15:27:48 +0300

    loading state

--- components/products/productDetails/index.jsx
+++ components/products/productDetails/index.jsx
@@ -1,16 +1,24 @@
 import { useEffect, useState } from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, ActivityIndicator } from "react-native";
 import { fetchProductDetails } from "../../../utils/products";
 
 const ProductDetails = ({ route, navigation }) => {
   const { itemIndex } = route.params;
   const [product, setProduct] = useState("");
+  const [loading, setLoading] = useState(true);
 
   const fetchProduct = async () => {};
 
   useEffect(() => {
     fetchProduct();
   }, []);
+  if (loading) {
+    return (
+      <View className="flex-1 items-center justify-center">
+        <ActivityIndicator size={"large"} />
+      </View>
+    );
+  }
   return (
     <View>
       <Text>ProductDetails {itemIndex}</Text>
