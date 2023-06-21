b99f69a33af7ac68b5e659a913bd9698ee4782fc
jonathan_baraza <jbaraza46@gmail.com>
2023-06-21 14:55:28 +0300

    modal input

--- components/products/productDetails/index.jsx
+++ components/products/productDetails/index.jsx
@@ -1,5 +1,12 @@
 import { useEffect, useState } from "react";
-import { View, Text, Button, ActivityIndicator, Image } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  ActivityIndicator,
+  Image,
+  Modal,
+} from "react-native";
 import { fetchProductDetails } from "../../../utils/products";
 import { useRoute, useNavigation } from "@react-navigation/native";
 const ProductDetails = () => {
@@ -10,6 +17,7 @@ const ProductDetails = () => {
 
   const [product, setProduct] = useState("");
   const [loading, setLoading] = useState(true);
+  const [modalVisible, setModalVisible] = useState(false);
 
   const fetchProduct = async () => {
     try {
@@ -52,55 +60,81 @@ const ProductDetails = () => {
   return (
     <View className="flex-1">
       {product ? (
-        <View>
-          <Text className="text-xl m-3 underline font-bold">
-            {product.title}
-          </Text>
-          <Image
-            resizeMode="contain"
-            className="w-full  h-[40vh]"
-            source={{ uri: product.images[0] }}
-          />
-          <View className="px-4">
-            <View className="">
-              <Text className="font-bold  text-base underline mb  mt-2">
-                Brand{" "}
-              </Text>
-              <Text className="text-gray-500  text-base"> {product.brand}</Text>
-            </View>
-            <View className="">
-              <Text className="font-bold  text-base underline mb  mt-2">
-                Category{" "}
-              </Text>
-              <Text className="text-gray-500  text-base">
-                {" "}
-                {product.category}
-              </Text>
-            </View>
-            <View className="">
-              <Text className="font-bold  text-base underline mb  mt-2">
-                Description{" "}
-              </Text>
-              <Text className="text-gray-500  text-base">
-                {" "}
-                {product.description}
-              </Text>
-            </View>
-            <View className="flex-row items-center mt-2">
-              <Text className="font-bold  text-base  ">Price</Text>
-              <Text className="text-gray-500 ml-1  text-base">
-                {" "}
-                Ksh{product.price}/=
-              </Text>
-            </View>
-            <View className="flex-row items-center mt-2">
-              <Text className="font-bold  text-base  ">Rating</Text>
-              <Text className="text-gray-500 ml-1 text-base">
-                {product.rating}/5.0
-              </Text>
+        <>
+          <View>
+            <Text className="text-xl m-3 underline font-bold">
+              {product.title}
+            </Text>
+            <Image
+              resizeMode="contain"
+              className="w-full  h-[40vh]"
+              source={{ uri: product.images[0] }}
+            />
+            <View className="px-4">
+              <View className="">
+                <Text className="font-bold  text-base underline mb  mt-2">
+                  Brand{" "}
+                </Text>
+                <Text className="text-gray-500  text-base">
+                  {" "}
+                  {product.brand}
+                </Text>
+              </View>
+              <View className="">
+                <Text className="font-bold  text-base underline mb  mt-2">
+                  Category{" "}
+                </Text>
+                <Text className="text-gray-500  text-base">
+                  {" "}
+                  {product.category}
+                </Text>
+              </View>
+              <View className="">
+                <Text className="font-bold  text-base underline mb  mt-2">
+                  Description{" "}
+                </Text>
+                <Text className="text-gray-500  text-base">
+                  {" "}
+                  {product.description}
+                </Text>
+              </View>
+              <View className="flex-row items-center mt-2">
+                <Text className="font-bold  text-base  ">Price</Text>
+                <Text className="text-gray-500 ml-1  text-base">
+                  {" "}
+                  Ksh{product.price}/=
+                </Text>
+              </View>
+              <View className="flex-row items-center mt-2">
+                <Text className="font-bold  text-base  ">Rating</Text>
+                <Text className="text-gray-500 ml-1 text-base">
+                  {product.rating}/5.0
+                </Text>
+              </View>
             </View>
           </View>
-        </View>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert("Modal has been closed.");
+              setModalVisible(!modalVisible);
+            }}
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.modalText}>Hello World!</Text>
+                <Pressable
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={() => setModalVisible(!modalVisible)}
+                >
+                  <Text style={styles.textStyle}>Hide Modal</Text>
+                </Pressable>
+              </View>
+            </View>
+          </Modal>
+        </>
       ) : (
         <View className="flex-1 items-center justify-center">
           <Text className="text-red-500">Could not fetch product</Text>
