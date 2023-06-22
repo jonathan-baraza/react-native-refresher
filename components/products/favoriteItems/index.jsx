c8d4d8dfa71fb16a3ac7d5fd410f46cc9f5ca6bb
jonathan_baraza <jbaraza46@gmail.com>
2023-06-22 12:47:33 +0300

    fix: product item length

--- components/products/favoriteItems/index.jsx
+++ components/products/favoriteItems/index.jsx
@@ -5,12 +5,20 @@ import ProductItem from "../ProductItem";
 const Favorites = () => {
   const { favoriteItems, addToFavorites } = useContext(Context);
   return (
-    <View className="flex-1 px-6 py-4">
-      <FlatList
-        data={favoriteItems}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <ProductItem product={item} />}
-      />
+    <View className="flex-1 px-6 ">
+      {favoriteItems.length > 0 ? (
+        <FlatList
+          data={favoriteItems}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <ProductItem product={item} />}
+        />
+      ) : (
+        <View className="flex-1 items-center justify-center px-4">
+          <Text className="text-yellow-500">
+            You have no favorite products currently.
+          </Text>
+        </View>
+      )}
     </View>
   );
 };
