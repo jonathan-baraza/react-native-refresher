2b503e964176e1f4e68ae17ddb169652584ea6a6
jonathan_baraza <jbaraza46@gmail.com>
2023-06-23 20:58:57 +0300

    box animation ref

--- screens/AnimationIndex.jsx
+++ screens/AnimationIndex.jsx
@@ -9,14 +9,17 @@ import {
 import { useRef } from "react";
 
 const AnimationIndex = () => {
-  const boxOpacityAnimationValue = useRef(new Animated.Value(0));
-  
+  const boxOpacityAnimationValue = useRef(new Animated.Value(0)).current;
+
   return (
     <View
       style={styles.container}
       className="flex-1 items-center justify-center"
     >
-      <Animated.View className="w-[200px] h-[200px] bg-red-400 mb-12"></Animated.View>
+      <Animated.View
+        ref={boxOpacityAnimationValue}
+        className="w-[200px] h-[200px] bg-red-400 mb-12"
+      ></Animated.View>
 
       <Button title="Fade this box p-3" />
     </View>
