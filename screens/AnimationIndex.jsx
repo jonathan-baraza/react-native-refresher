e4cd377af720bde38b309be63f23e769e17acee7
jonathan_baraza <jbaraza46@gmail.com>
2023-06-23 21:02:00 +0300

    fin: fade in animation for the box

--- screens/AnimationIndex.jsx
+++ screens/AnimationIndex.jsx
@@ -11,17 +11,31 @@ import { useRef } from "react";
 const AnimationIndex = () => {
   const boxOpacityAnimationValue = useRef(new Animated.Value(0)).current;
 
+  const handleFadeInBox = () => {
+    Animated.timing(boxOpacityAnimationValue, {
+      duration: 1000,
+      toValue: 1,
+      delay: 200,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const opacityStyle = {
+    opacity: boxOpacityAnimationValue,
+  };
+
   return (
     <View
       style={styles.container}
       className="flex-1 items-center justify-center"
     >
       <Animated.View
+        style={opacityStyle}
         ref={boxOpacityAnimationValue}
         className="w-[200px] h-[200px] bg-red-400 mb-12"
       ></Animated.View>
 
-      <Button title="Fade this box p-3" />
+      <Button onPress={handleFadeInBox} title="Fade this box p-3" />
     </View>
   );
 };
